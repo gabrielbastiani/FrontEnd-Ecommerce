35946d5a4d504c902bb679421ef728a6b321649c
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-14 16:28:07 -0300

    'frontend'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  nextScriptWorkers: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Loja Virtual Builder Seu Negócio Online</title>
+      </Head>
+      <div>
+        <h1>E-commerce</h1>
+      </div>
+    </>
+  )
+}
