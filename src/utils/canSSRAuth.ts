2e97e2e78ded45d4d1b10a5ded5a3128aae98177
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-14 17:15:55 -0300

    'loja'

---
+++ src/utils/canSSRAuth.ts
@@ -0,0 +1,41 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
+import { destroyCookie, parseCookies } from '../../node_modules/nookies/dist/index';
+import { AuthTokenError } from '../services/errors/AuthTokenError'
+
+
+//funcao para paginas que só users logados podem ter acesso.
+export function canSSRAuth<P>(fn: GetServerSideProps<P>){
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);    
+
+    const token = cookies['@lojavirtual.token'];
+
+    if(!token){
+      return{
+        redirect:{
+          destination: '/login',
+          permanent: false,
+        }
+      }
+    }
+
+    try{
+      return await fn(ctx);
+    }catch(err){
+      if(err instanceof AuthTokenError){
+        destroyCookie(ctx, '@lojavirtual.token');
+
+        return{
+          redirect:{
+            destination: '/login',
+            permanent: false
+          }
+        }
+
+      }
+    }
+
+
+  }
+
+}
