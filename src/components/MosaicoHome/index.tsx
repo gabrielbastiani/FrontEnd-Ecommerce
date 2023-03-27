66e8ca150c9e16427c5038afa92c5f3922cb8587
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-27 17:29:04 -0300

    'foto'

--- src/components/MosaicoHome/index.tsx
+++ src/components/MosaicoHome/index.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
-
-
-
 import {
     MosaicoHomeSection,
     MosaicoHome1,
     MosaicoHome2,
     MosaicoHome3,
-    MosaicoHome4
+    MosaicoHome4,
+    GridContatinerTop,
+    GridContatinerBotton
 } from './styles';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -16,16 +15,14 @@ import { setupAPIClient } from '../../services/api';
 
 export const MosaicoHome = () => {
 
-    const [bannersHome, setBannersHome] = useState([]);
-
-    console.log(bannersHome.map((Item)) => {Item.url})
+    const [mosaicoBanners, setMosaicoBanners] = useState([]);
 
     useEffect(() => {
         async function loadHomeBanner() {
             const apiClient = setupAPIClient();
             try {
                 const response = await apiClient.get(`/activeBannerMosaico`);
-                setBannersHome(response.data || []);
+                setMosaicoBanners(response.data || []);
             } catch (error) {
                 console.log(error.response.data)
             }
@@ -35,26 +32,46 @@ export const MosaicoHome = () => {
 
     return (
         <MosaicoHomeSection>
-            <MosaicoHome1>
-                <Link href={''}>
-                    <Image src={'http://localhost:3333/files/'} width={618} height={300} alt="Mosaico Home Loja" />
-                </Link>
-            </MosaicoHome1>
-            <MosaicoHome2>
-                <Link href=''>
-                    <Image src={'http://localhost:3333/files/'} width={618} height={300} alt="Mosaico Home Loja" />
-                </Link>
-            </MosaicoHome2>
-            <MosaicoHome3>
-                <Link href=''>
-                    <Image src={'http://localhost:3333/files/'} width={828} height={402} alt="Mosaico Home Loja" />
-                </Link>
-            </MosaicoHome3>
-            <MosaicoHome4>
-                <Link href=''>
-                    <Image src={'http://localhost:3333/files/'} width={404} height={402} alt="Mosaico Home Loja" />
-                </Link>
-            </MosaicoHome4>
+            <GridContatinerTop>
+                {mosaicoBanners.splice(3).map((mos1) => {
+                    return (
+                        <MosaicoHome1 key={mos1.id}>
+                            <Link href={mos1.url}>
+                                <Image src={'http://localhost:3333/files/' + mos1.bannerMosaico} width={650} height={300} alt="Mosaico Home Loja" />
+                            </Link>
+                        </MosaicoHome1>
+                    )
+                })}
+                {mosaicoBanners.splice(2).map((mos2) => {
+                    return (
+                        <MosaicoHome2 key={mos2.id}>
+                            <Link href={mos2.url}>
+                                <Image src={'http://localhost:3333/files/' + mos2.bannerMosaico} width={650} height={300} alt="Mosaico Home Loja" />
+                            </Link>
+                        </MosaicoHome2>
+                    )
+                })}
+            </GridContatinerTop>
+            <GridContatinerBotton>
+                {mosaicoBanners.splice(1).map((mos3) => {
+                    return (
+                        <MosaicoHome3 key={mos3.id}>
+                            <Link href={mos3.url}>
+                                <Image src={'http://localhost:3333/files/' + mos3.bannerMosaico} width={858} height={402} alt="Mosaico Home Loja" />
+                            </Link>
+                        </MosaicoHome3>
+                    )
+                })}
+                {mosaicoBanners.splice(0).map((mos4) => {
+                    return (
+                        <MosaicoHome4 key={mos4.id}>
+                            <Link href={mos4.url}>
+                                <Image src={'http://localhost:3333/files/' + mos4.bannerMosaico} width={440} height={402} alt="Mosaico Home Loja" />
+                            </Link>
+                        </MosaicoHome4>
+                    )
+                })}
+            </GridContatinerBotton>
         </MosaicoHomeSection>
     )
 }
--- src/components/MosaicoHome/styles.ts
+++ src/components/MosaicoHome/styles.ts
@@ -1,29 +1,105 @@
 import styled from 'styled-components'
 
 export const MosaicoHomeSection = styled.section`
-  width: 1250px;
-  margin: 50px auto;
+  width: 1300px;
+  margin: auto;
+
+  @media (max-width: 1360px) {
+    width: 1000px;
+    margin: 0 50px;
+  }
+`
+
+export const GridContatinerTop = styled.div`
+  display: grid;
+  grid-template-columns: 650px 650px;
+  gap: 10px;
+
+  @media (max-width: 1360px) {
+    grid-template-columns: 550px 550px;
+  }
+
+  @media (max-width: 1182px) {
+    grid-template-columns: 450px 450px;
+  }
+
+  @media (max-width: 1049px) {
+    grid-template-columns: 758px;
+  }
 `
 
 export const MosaicoHome1 = styled.div`
-  margin-right: 5px;
-  width: 618px;
-  display: inline-block;
+  text-align: center;
+  padding: 20px 0;
+
+  img {
+    @media (max-width: 1360px) {
+      width: 550px;
+    }
+
+    @media (max-width: 1182px) {
+      width: 450px;
+    }
+  }
 `
 
 export const MosaicoHome2 = styled.div`
-  width: 618px;
-  display: inline-block;
-  margin-left: 5px;
+  text-align: center;
+  padding: 20px 0;
+
+  img {
+    @media (max-width: 1360px) {
+      width: 550px;
+    }
+
+    @media (max-width: 1182px) {
+      width: 450px;
+    }
+  }
+`
+
+export const GridContatinerBotton = styled.div`
+  display: grid;
+  grid-template-columns: 858px 340px;
+  gap: 10px;
+
+  @media (max-width: 1360px) {
+    grid-template-columns: 758px 240px;
+  }
+
+  @media (max-width: 1182px) {
+    grid-template-columns: 658px 140px;
+  }
+
+  @media (max-width: 1049px) {
+    grid-template-columns: 758px;
+  }
 `
 
 export const MosaicoHome3 = styled.div`
-  width: 828px;
-  display: inline-block;
+  text-align: center;
+
+  img {
+    @media (max-width: 1360px) {
+      width: 758px;
+    }
+
+    @media (max-width: 1182px) {
+      width: 658px;
+    }
+  }
 `
 
 export const MosaicoHome4 = styled.div`
-  width: 404px;
-  display: inline-block;
-  margin-left: 10px;
+  text-align: center;
+
+  img {
+    @media (max-width: 1360px) {
+      width: 340px;
+    }
+
+    @media (max-width: 1182px) {
+      width: 240px;
+    }
+  }
 `
