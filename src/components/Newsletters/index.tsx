ad08bdb2649e731c650ba07adfc4c4f6e15428b4
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-30 16:55:27 -0300

    'loja'

--- src/components/Newsletters/index.tsx
+++ src/components/Newsletters/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
     SectionContato,
     BlockContato,
@@ -14,6 +14,21 @@ const Newsletters = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [loja_id, setLoja_id] = useState('');
+
+    useEffect(() => {
+        async function loadStore() {
+            const apiClient = setupAPIClient();
+            try {
+                const response = await apiClient.get(`/loja`);
+                setLoja_id(response.data.id || "");
+                
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        loadStore();
+    }, []);
 
     function isEmail(email: string) {
         return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
@@ -37,7 +52,7 @@ const Newsletters = () => {
             }
 
             const apiClient = setupAPIClient();
-            const response = await apiClient.post('/createNewsletter', {name: name, email: email});
+            await apiClient.post('/createNewsletter', {name: name, email: email, loja_id: loja_id});
 
             toast.success('Dados enviados com sucesso!');
 
