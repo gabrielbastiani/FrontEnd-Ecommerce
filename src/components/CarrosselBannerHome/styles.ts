2e4c04bd8d4ab69e0b407666753b741054b04190
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-04-25 20:51:12 -0300

    categorys

--- src/components/CarrosselBannerHome/styles.ts
+++ src/components/CarrosselBannerHome/styles.ts
@@ -21,7 +21,7 @@ export const Container = styled.section`
     width: 100vw;
     height: 100%;
     object-fit: contain;
-    margin-top: 128px;
+    margin-top: 120px;
 
     @media (max-width: 900px) {
       margin-top: 200px;
