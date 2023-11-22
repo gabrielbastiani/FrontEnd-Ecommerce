7f660c90d54e3f12739971fa15b23276ae9b30c4
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-11-22 10:55:16 -0300

    orders

--- src/components/ChatLive/index.tsx
+++ src/components/ChatLive/index.tsx
@@ -19,22 +19,14 @@ const ChatLive = () => {
         reloadsConfigs();
     }, []);
 
+    console.log(datasConfigs[0]?.code_live_chat_tawkTo)
+
 
     return (
         <ChatsLive>
             <script
                 dangerouslySetInnerHTML={{
-                    __html: `
-                    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                    (function(){
-                        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                        s1.async=true;
-                        s1.src='${datasConfigs[0]?.code_live_chat_tawkTo}';
-                        s1.charset='UTF-8';
-                        s1.setAttribute('crossorigin','*');
-                        s0.parentNode.insertBefore(s1,s0);
-                    })();
-                    `,
+                    __html: datasConfigs[0]?.code_live_chat_tawkTo,
                 }}
             />
         </ChatsLive>
