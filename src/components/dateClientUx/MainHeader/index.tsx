ecbf9c5df39578ec101345121d67e73877b7708e
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-12-01 15:25:46 -0300

    orders

--- src/components/dateClientUx/MainHeader/index.tsx
+++ src/components/dateClientUx/MainHeader/index.tsx
@@ -1,16 +1,122 @@
-import React, { useContext, useMemo, useState } from 'react';
-import { 
-    Container, 
-    Profile, 
-    Welcome, 
-    UserName
-}  from './styles';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import {
+    Container,
+    Profile,
+    Welcome,
+    UserName,
+    NotificationBell,
+    DropDownContent,
+    Title,
+    BlockButtonsNotification,
+    AllViewd,
+    ClearNotifications,
+    LinkNotification,
+    BlockNotification,
+    BoxIcons,
+    Block,
+    Viewed,
+    Menssages,
+    ClockBlock,
+    DataNotification
+} from './styles';
 import { AuthContext } from '../../../contexts/AuthContext';
+import { FaBell, FaCartPlus, FaRegBell, FaRegClock } from 'react-icons/fa';
+import { AmountItens } from '../../HeaderStore/styles';
+import { setupAPIClient } from '../../../services/api';
+import { Avisos } from '../../Avisos';
+import moment from 'moment';
 
 
 export const MainHeader = () => {
 
     const { customer } = useContext(AuthContext);
+
+    const [notifications, setNotifications] = useState<any[]>([]);
+    const [openNotification, setOpenNotification] = useState(true);
+    const [viewd, setViewd] = useState<any[]>([]);
+    const [newFalse, setNewFalse] = useState(Number);
+
+    const handleNotificationOpen = () => {
+        setOpenNotification(!openNotification);
+    }
+
+    useEffect(() => {
+        function loadNumberNotifications() {
+            const booleanArray = viewd.map(fal => fal.viewed);
+            const falseValues = booleanArray.filter(value => value === false);
+            setNewFalse(falseValues.length);
+        }
+        loadNumberNotifications();
+    }, [viewd]);
+
+    useEffect(() => {
+        async function findNotificationsCustomer() {
+            try {
+                const apiClient = setupAPIClient();
+                const { data } = await apiClient.get(`/notificationPainelCustomer?customer_id=${customer?.id}`);
+
+                setNotifications(data.slice(0, 15));
+                setViewd(data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        findNotificationsCustomer();
+    }, [customer?.id]);
+
+    async function findNotificationsCustomer() {
+        try {
+            const apiClient = setupAPIClient();
+            const { data } = await apiClient.get(`/notificationPainelCustomer?customer_id=${customer?.id}`);
+
+            setNotifications(data.slice(0, 15));
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function loadNumberNotifications() {
+        const booleanArray = viewd.map(fal => fal.viewed);
+        const falseValues = booleanArray.filter(value => value === false);
+        setNewFalse(falseValues.length);
+    }
+
+    async function notificationsViewd(id: string) {
+        try {
+            const apiClient = setupAPIClient();
+            await apiClient.put(`/updateViewdNotificationCustomer?notificationCustomer_id=${id}&customer_id=${customer?.id}`);
+            loadNumberNotifications();
+            findNotificationsCustomer();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function notificationsAllViewd() {
+        try {
+            const apiClient = setupAPIClient();
+            await apiClient.put(`/updateAllViewdNotificationCustomer?customer_id=${customer?.id}`);
+            loadNumberNotifications();
+            findNotificationsCustomer();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function clearAllnotifications() {
+        try {
+            const apiClient = setupAPIClient();
+            await apiClient.put(`/clearAllNotificationsCustomer?customer_id=${customer?.id}`);
+            loadNumberNotifications();
+            findNotificationsCustomer();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
 
     return (
         <Container>
@@ -18,6 +124,79 @@ export const MainHeader = () => {
                 <Welcome>Olá</Welcome>
                 <UserName>{customer?.name}</UserName>
             </Profile>
+
+            <NotificationBell onClick={handleNotificationOpen}>
+                {newFalse === 0 ?
+                    <FaRegBell size={20} />
+                    :
+                    <>
+                        <AmountItens>
+                            <span>{newFalse}</span>
+                        </AmountItens>
+                        <FaBell size={20} />
+                    </>
+                }
+            </NotificationBell>
+
+            {openNotification ?
+                null
+                :
+                <>
+                    <DropDownContent>
+                        <Title>Notificações</Title>
+                        <BlockButtonsNotification>
+                            <AllViewd
+                                onClick={notificationsAllViewd}
+                            >
+                                Marcar todas como lidas
+                            </AllViewd>
+                            <ClearNotifications
+                                onClick={clearAllnotifications}
+                            >
+                                Limpar todas notificações
+                            </ClearNotifications>
+                        </BlockButtonsNotification>
+
+                        {notifications.length === 0 ?
+                            <Avisos texto='Sem notificações ainda...' />
+                            :
+                            <>
+                                {notifications.map((item, index) => {
+                                    return (
+                                        <>
+                                            {item.block_bell === true ?
+                                                <LinkNotification
+                                                    key={index}
+                                                    href={item.link}
+                                                    onClick={() => notificationsViewd(item.id)}
+                                                >
+                                                    <BlockNotification style={{ background: item.viewed === true ? '#ff000052' : 'unset' }}>
+                                                        <BoxIcons>
+                                                            <FaCartPlus size={28} />
+                                                        </BoxIcons>
+                                                        <Block>
+                                                            {item.viewed === true ? <Viewed>VISUALIZADA</Viewed> : null}
+                                                            <Menssages
+                                                                dangerouslySetInnerHTML={{ __html: item.message }}
+                                                            ></Menssages>
+                                                            <ClockBlock>
+                                                                <FaRegClock size={20} />
+                                                                <DataNotification>{moment(item.created_at).format('DD/MM/YYYY - HH:mm')}</DataNotification>
+                                                            </ClockBlock>
+                                                        </Block>
+                                                    </BlockNotification>
+                                                </LinkNotification>
+                                                :
+                                                null
+                                            }
+                                        </>
+                                    )
+                                })}
+                            </>
+                        }
+                    </DropDownContent>
+                </>
+            }
         </Container>
     );
 }
--- src/components/dateClientUx/MainHeader/styles.ts
+++ src/components/dateClientUx/MainHeader/styles.ts
@@ -24,3 +24,93 @@ export const UserName = styled.span`
   text-decoration: none;
   color: ${(props) => props?.theme?.colors?.black};
 `;
+
+export const NotificationBell = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px;
+  cursor: pointer;
+
+  svg {
+    color: ${(props) => props.theme.colors.black};
+  }
+`;
+
+export const DropDownContent = styled.div`
+  height: 58vh;
+  overflow-y: auto;
+  right: 0;
+  top: 170px;
+  padding: 20px;
+  position: absolute;
+  background-color: ${(props) => props?.theme?.colors?.white};
+  color: ${(props) => props?.theme?.colors?.black};
+  width: 33%;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.7);
+  z-index: 99;
+`;
+
+export const Title = styled.h2`
+  color: ${(props) => props?.theme?.colors?.black};
+  margin-bottom: 25px;
+`;
+
+export const BlockButtonsNotification = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const AllViewd = styled.button`
+  background: transparent;
+  color: ${(props) => props.theme.colors.black};
+`;
+
+export const ClearNotifications = styled.button`
+  background-color: ${(props) => props.theme.colors.warning};
+  color: ${(props) => props.theme.colors.white};
+  padding: 8px;
+`;
+
+export const LinkNotification = styled.a`
+  color: inherit;
+`;
+
+export const BlockNotification = styled.div`
+  border: 1px solid ${(props) => props?.theme?.colors?.black};
+  margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin: 13px;
+  }
+`;
+
+export const BoxIcons = styled.div``;
+
+export const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+export const Menssages = styled.span`
+  padding-bottom: 12px;
+  color: ${(props) => props.theme.colors.black};
+`;
+
+export const ClockBlock = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const DataNotification = styled.span`
+  color: ${(props) => props.theme.colors.black};
+`;
+
+export const Viewed = styled.strong`
+  color: ${(props) => props.theme.colors.warning};
+  font-style: italic;
+  padding-bottom: 7px;
+`;
